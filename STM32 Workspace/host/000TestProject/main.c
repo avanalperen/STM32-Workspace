f744e2b8ba0615739cc1e33e05223b099e5eec93
Alperen Avan <alperen_0150@hotmail.com>
2024-08-30 17:53:37 +0300

    Initial STM32 Workspace Commit

---
+++ STM32 Workspace/host/000TestProject/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 29, 2024
+ *      Author: Alperen
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("Hello World!");
+
+	getchar();
+
+	return 0;
+}
+
+
