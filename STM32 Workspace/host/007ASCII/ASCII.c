3c5f493b130167fbc95e130865072a4dd077e383
Alperen Avan <alperen_0150@hotmail.com>
2024-10-17 22:42:37 +0300

    ASCII 2

--- STM32 Workspace/host/007ASCII/ASCII.c
+++ STM32 Workspace/host/007ASCII/ASCII.c
@@ -5,10 +5,29 @@
  *      Author: Alperen
  */
 
-include <stdio.h>
+#include <stdio.h>
 
 int main(void)
 {
+	char c1,c2,c3,c4,c5,c6;
 
+	printf("Enter any 6 characters of your choice : ");
+
+	//scanf("%c %c %c %c %c %c",&c1,&c2,&c3,&c4,&c5,&c6);
+
+	for(int i=0;i<6;i++)
+	{
+		getchar()
+	}
+
+	printf("\nASCII codes : %u,%u,%u,%u,%u,%u\n",c1,c2,c3,c4,c5,c6);
+
+	printf("Pres enter key to exit the application \n");
+
+	while(getchar() != '\n')
+	{
+		//just read the input buffer and do nothing
+	}
+	getchar();
 }
 
